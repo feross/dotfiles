9dbce4e1e166b727f72be95bdd4844811ceb71e9
Feross Aboukhadijeh <feross@feross.org>
2015-11-02 12:51:16 -0800

    add "bin/linode-backup.sh"

---
+++ bin/linode-backup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+rsync -a -O -v --delete -e "ssh -p 44444" feross@box:"/home/feross/" /Volumes/Zelda/Backup/box
+rsync -a -O -v --delete -e "ssh -p 44444" feross@future:"/home/feross/" /Volumes/Zelda/Backup/future
+rsync -a -O -v --delete -e "ssh -p 44444" feross@athena:"/home/feross/" /Volumes/Zelda/Backup/athena
+rsync -a -O -v --delete -e "ssh -p 44444" feross@gimli:"/home/feross/" /Volumes/Zelda/Backup/gimli
+rsync -a -O -v --delete -e "ssh -p 44444" feross@webtorrent:"/home/feross/" /Volumes/Zelda/Backup/webtorrent
