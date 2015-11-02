808336ebce410357eca6e6583e9bfd4ed8bb5de9
Feross Aboukhadijeh <feross@feross.org>
2015-11-02 13:34:24 -0800

    exclude *.log from linode backup

--- bin/linode-backup.sh
+++ bin/linode-backup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-set -e
 
-rsync -a -O -v --delete -e "ssh -p 44444" feross@box:"/home/feross/" /Volumes/Zelda/Backup/box
-rsync -a -O -v --delete -e "ssh -p 44444" feross@future:"/home/feross/" /Volumes/Zelda/Backup/future
-rsync -a -O -v --delete -e "ssh -p 44444" feross@athena:"/home/feross/" /Volumes/Zelda/Backup/athena
-rsync -a -O -v --delete -e "ssh -p 44444" feross@gimli:"/home/feross/" /Volumes/Zelda/Backup/gimli
-rsync -a -O -v --delete -e "ssh -p 44444" feross@webtorrent:"/home/feross/" /Volumes/Zelda/Backup/webtorrent
+rsync -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@box:"/home/feross/" /Volumes/Zelda/Backup/box
+rsync -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@future:"/home/feross/" /Volumes/Zelda/Backup/future
+rsync -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@athena:"/home/feross/" /Volumes/Zelda/Backup/athena
+rsync -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@gimli:"/home/feross/" /Volumes/Zelda/Backup/gimli
+rsync -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@webtorrent:"/home/feross/" /Volumes/Zelda/Backup/webtorrent
