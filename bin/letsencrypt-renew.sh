4882a1252393f8ce3c57852cab3c622254d3bb1f
Feross Aboukhadijeh <feross@feross.org>
2017-02-08 02:16:32 -0800

    stop/start apache2 in case that's used

--- bin/letsencrypt-renew.sh
+++ bin/letsencrypt-renew.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Renew certificates automatically before they expire. Let's Encrypt
-# certificates last for 90 days, so this command should run more 
-# frequently than that (docs recommend twice a day).
+# certificates last for 90 days, so this command should run more
+# frequently than that (say, daily).
 
 sudo service nginx stop
-sudo letsencrypt renew --agree-tos 
+sudo service apache2 stop
+sudo letsencrypt renew --agree-tos
 sudo service nginx start
+sudo service apache2 start
