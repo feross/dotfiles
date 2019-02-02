cbd6ffc932f1539dc685776aee6a4d1703b05f7c
Feross Aboukhadijeh <feross@feross.org>
2019-02-01 19:49:40 -0800

    remove rsync box

--- bin/linode-backup.sh
+++ bin/linode-backup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-rsync -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@box:"/home/feross/" /Volumes/Time\ Machine\ 4TB/Linode/box
 rsync --inplace -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@future:"/home/feross/" /Volumes/Time\ Machine\ 4TB/Linode/future
 rsync --inplace -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@athena:"/home/feross/" /Volumes/Time\ Machine\ 4TB/Linode/athena
 rsync --inplace -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@gimli:"/home/feross/" /Volumes/Time\ Machine\ 4TB/Linode/gimli
