84fceb88bbfcfaf87db1df73a8c4c82b7499047f
Feross Aboukhadijeh <feross@feross.org>
2019-02-01 21:16:58 -0800

    update scripts

--- bin/linode-backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rsync --inplace -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@future:"/home/feross/" /Volumes/Time\ Machine\ 4TB/Linode/future
-rsync --inplace -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@athena:"/home/feross/" /Volumes/Time\ Machine\ 4TB/Linode/athena
-rsync --inplace -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@gimli:"/home/feross/" /Volumes/Time\ Machine\ 4TB/Linode/gimli
-rsync --inplace -a -O -v --exclude "*.log" --delete -e "ssh -p 44444" feross@webtorrent:"/home/feross/" /Volumes/Time\ Machine\ 4TB/Linode/webtorrent
